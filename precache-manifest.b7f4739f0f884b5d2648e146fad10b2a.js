5802e4ba51341cb361accb4f9071f61369e365a8
Joseph May <jwmay87@gmail.com>
2022-10-12 17:54:46 -0700

    Updates

---
+++ precache-manifest.b7f4739f0f884b5d2648e146fad10b2a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d5f02b98e7be0dcb73ee3590d819253",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df98fa85f0c78476ed60",
+    "url": "/static/js/2.6724a612.chunk.js"
+  },
+  {
+    "revision": "75b844983861a94a73806fceab2d936f",
+    "url": "/static/js/2.6724a612.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01dedd6ba47f65e48fa4",
+    "url": "/static/js/main.643a1879.chunk.js"
+  },
+  {
+    "revision": "d58716498dcabb61c8f6",
+    "url": "/static/js/runtime-main.7b046f31.js"
+  },
+  {
+    "revision": "30174016b9f7a62f0b79fecbd0ba94a9",
+    "url": "/static/media/chem-cash-logo.30174016.png"
+  },
+  {
+    "revision": "11d1d297709dd97670639184d56fffeb",
+    "url": "/static/media/chemistry-background.11d1d297.jpg"
+  },
+  {
+    "revision": "f927335d3f642de54d361e01ad3c4527",
+    "url": "/static/media/doc-may-profile.f927335d.jpg"
+  },
+  {
+    "revision": "cfcd4ce6646adc78b2e3270c2b62c688",
+    "url": "/static/media/songs-background.cfcd4ce6.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ac10e5280f38e5fdc02eb2409e2fe0d1.js"
+  "/precache-manifest.b7f4739f0f884b5d2648e146fad10b2a.js"
 );
 
 self.addEventListener('message', (event) => {
